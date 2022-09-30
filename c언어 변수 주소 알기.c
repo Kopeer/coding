a0993fe74ba096aabd0d64e41c59b55e5da05675
Hello <108929116+Kopeer@users.noreply.github.com>
2022-09-30 18:21:12 +0900

    Create c언어 변수 주소 알기.c

---
+++ c언어 변수 주소 알기.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+void main(int argc, char *argv[]) {
+	int a;
+	
+	a = 153;
+	printf("변수 a의 주소 : %d\n", &a);
+}
