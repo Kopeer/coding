fb5c2a035c5b26b284479334711ed9417381dd6d
Hello <108929116+Kopeer@users.noreply.github.com>
2022-09-30 18:47:18 +0900

    Create c언어 포인터 중요 포인트.c

---
+++ c언어 포인터 중요 포인트.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+void main(int argc, char *argv[]) {
+	int a;
+	int * p = &a
+	*p = *p + 1 
+}
