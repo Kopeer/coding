a5639d5d4c23db4131ccba16d798d74543da358b
Hello <108929116+Kopeer@users.noreply.github.com>
2022-11-11 17:51:31 +0900

    Create c언어 defind & 헤더파일.c

---
+++ c언어 defind & 헤더파일.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "a.h"
+#define p printf
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	p("%d\n", sum(10, 20));
+	return 0;
+}
